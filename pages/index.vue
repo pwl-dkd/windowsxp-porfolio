f1f44dea996673333c01620b07af6746b5873bfd
Tim <34155548+TotallyTheTim@users.noreply.github.com>
2020-08-01 16:05:04 +0200

    Tim's changes

--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,46 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        windowsxp-porfolio
-      </h1>
-      <h2 class="subtitle">
-        Pawel portfolio project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div id="main">
+    <desktop></desktop>
+    <navbar></navbar>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import desktop from '~/components/desktop.vue'
+import navbar from '~/components/navbar.vue'
 
 export default {
   components: {
-    Logo
+    desktop,
+    navbar
   }
 }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
+<style lang="scss">
+#main {
+  width: 100vw;
+  position: fixed;
+  height: 100vh;
   display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+  flex-direction: column;
+  font-family: tahoma, sans-serif;
+  #desktop {
+    background-image: url(~@/assets/background/background.jpg);
+    background-size: cover;
+    background-position: center;
+    flex: 1;
+  }
+  #navbar {
+    background-color: #245edc;
+    background-image: linear-gradient(
+      to bottom,
+      #4993e6,
+      transparent 12px,
+      transparent calc(100% - 12px),
+      #1941a5
+    );
+    flex: 0 0 30px;
+  }
 }
 </style>
